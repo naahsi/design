41243911b7d204ca01cad8e32d3fcb130b734632
naahsi <39004122+naahsi@users.noreply.github.com>
2020-04-30 14:15:53 +0200

    Update site.js

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -1,15 +1,15 @@
 window.addEventListener('scroll', function() {
-  const wipStr = document.getElementById('wip-start');
-  const workHdSp = document.querySelectorAll('h2 > span')[0];
-  const sevVh = (window.innerHeight / 100) * 7;
-  const realTop = wipStr.offsetTop - sevVh;
+  let wipStr = document.getElementById('wip-start'),
+      workHdSp = document.querySelectorAll('h2 > span')[0],
+      sevVh = (window.innerHeight / 100) * 7,
+      realTop = wipStr.offsetTop - sevVh;
 
   if (realTop < document.body.scrollTop) {
     workHdSp.setAttribute('style', 'opacity: 0.7;');
-    console.log('working');
+    console.log(sevVh);
   } else {
     workHdSp.setAttribute('style', 'opacity: 0;');
-    console.log('not working');
+    console.log(realTop);
   }
 });
 
