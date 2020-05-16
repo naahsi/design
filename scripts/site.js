462870813be9572c43bce1e47b8207e8b9428e7f
Ishaan Sinha <ishaan.sinhaa@gmail.com>
2020-05-16 21:25:29 +0200

    up-to-date

--- scripts/site.js
+++ scripts/site.js
@@ -1,9 +1,11 @@
+// Variables
 let copy = document.getElementById('copy-email'),
     notify = document.getElementById('copy-notification'),
     workTop = document.getElementById('work').offsetTop,
     toWork = document.querySelectorAll('.to-work path')[0],
     toTop = document.querySelectorAll('.to-top path')[0];
 
+// Copy to clipboard
 const clickCopy = (e) => {
   if (e.target == copy) {
     clipboard.writeText('ishaan.sinhaa@gmail.com')
@@ -16,23 +18,27 @@ const clickCopy = (e) => {
   }
 };
 
+// Go to Work
 const goToWork = (e) => {
   if (e.target == toWork) {
     window.scroll({ top: workTop, left: 0, behavior: 'smooth' });
   }
 };
 
+// Back to top
 const backToTop = (e) => {
   if (e.target == toTop) {
     window.scroll({ top: 0, left: 0, behavior: 'smooth' });
   }
 };
 
+// Event listeners
 window.addEventListener('click', (e) => {
   clickCopy(e);
   goToWork(e);
   backToTop(e);
 });
+
 window.addEventListener('touchend', (e) => {
   clickCopy(e);
   goToWork(e);
